51fa3ec4f599a6b0e029d05bf4eb6b205ad3679d
eppakayala sai krusheel <36036181+saikrusheel19@users.noreply.github.com>
2019-06-11 15:17:10 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,7 +3,6 @@
 int main()
 {
     char ch;
-    printf("Enter any character: ");
     scanf("%c", &ch);
     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ||
        ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
