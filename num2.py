43bdad2f65d5243b85dfde6b057050369d4404da
eppakayala sai krusheel <36036181+saikrusheel19@users.noreply.github.com>
2019-07-16 12:08:59 +0530

    Update num2.py

--- num2.py
+++ num2.py
@@ -1,8 +1,8 @@
-a,b=map(int,input().split())
+j,k=map(int,input().split())
 n=list(map(int,input().split()))
 m=[]
-for a in range(b):
-       j,k=map(int,input().split())
-       m.append(sum(n[j-1:k]))
-for j in m:
+for j in range(k):
+       a,b=map(int,input().split())
+       m.append(sum(n[a-1:b]))
+for a in m:
        print(j)
